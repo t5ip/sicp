76a349820c85fa1afcf5731f999349c4531654ea
t5ip <t5ip@users.noreply.github.com>
2018-03-05 01:16:35 +0200

    1.16

--- chapter1/1.16.scm
+++ chapter1/1.16.scm
@@ -1,12 +1,12 @@
 (define (iter-exp b n a)
   (display a) (newline)
-  (cond  ((= n 1) a)
-	 ((even? n) (iter-exp (square b) (/ n 2) (square b)))
-	 (else (* b (iter-exp b (- n 1) a))))) 
+  (cond  ((= n 0) a)
+	 ((even? n) (iter-exp (square b) (/ n 2) a))
+	 (else (iter-exp b (- n 1) (* a b)))))
 
 (define (even? n)
   (= (remainder n 2) 0))
 
-(iter-exp 5 24 1)
+(iter-exp 2 13 1)
 
 
