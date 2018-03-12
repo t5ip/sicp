790f4eab1b3f9a9d1177bc0b959c895a966015d4
t5ip <t5ip@users.noreply.github.com>
2018-03-12 20:32:59 +0200

    1.21

---
+++ chapter1/1.21.scm
@@ -0,0 +1,12 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n a)
+  (cond ((> (square a) n) n)
+        ((divides? n a) a)
+	(else (find-divisor n (+ a 1)))))
+
+(define (divides? a b)
+  (= (remainder a b) 0))
+
+(smallest-divisor (* 17 13))
