6031761d75610bec8e772bc396dc385632ede701
t5ip <t5ip@users.noreply.github.com>
2018-03-17 00:31:21 +0200

    1.29

--- chapter1/1.29.scm
+++ chapter1/1.29.scm
@@ -7,21 +7,21 @@
 (define (cube x)
   (* x x x))
 
-(define (simpsons-rule term a b n)
+(define (simpsons-rule f a b n)
   (define (h)
     (/ (- b a) n))
-  (define (simpsons-next x)
-    (+ x (h)))
-  (define (simpsons-common-term x)
-    (* (/ (h) 3) (term x)))
-  (define (simpsons-term x)
-    (cond ((= x a) (simpsons-common-term x))
-	  ((= x b) (simpsons-common-term x))
-	  ((even? (round (/ (- x a) (h)))) (* 2 (simpsons-common-term x)))
-	  (else (* 4 (simpsons-common-term x)))))
-  (sum simpsons-term a simpsons-next b))
+  (define (simpsons-next k)
+    (+ k 1))
+  (define (simpsons-common-term k)
+    (* (/ (h) 3) (f (+ a (* k (h))))))
+  (define (simpsons-term k)
+    (cond ((= k 0) (simpsons-common-term k))
+	  ((= k n) (simpsons-common-term k))
+	  ((even? k) (* 2 (simpsons-common-term k)))
+	  (else (* 4 (simpsons-common-term k)))))
+  (sum simpsons-term 0 simpsons-next n))
   
-(simpsons-rule cube 0.0 1.0 1000) 
+(simpsons-rule cube 0.0 1.0 100) 
 
 
 
